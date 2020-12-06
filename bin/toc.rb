e9df8f955f9b86884c2e6fe15bb517aa78dbe4cb
Billiam <billiamthesecond@gmail.com>
2020-12-06 00:11:34 -0600

    Move file first

--- bin/toc.rb
+++ bin/toc.rb
@@ -4,6 +4,6 @@ require 'yaml'
 files = Dir.glob('*.ipynb')
 out = {}
 docs = files.map {|f| File.basename(f, '.*') }.sort
-out['sections'] = docs.map {|doc| { 'file' => doc }}
 out['file'] = docs.last
-puts YAML.dump(out)
+out['sections'] = docs.map {|doc| { 'file' => doc }}
+puts YAML.dump(out, indentation: 2)
